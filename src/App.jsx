07faea44b4afee72c20b32ad548a30fc2e46fe2b
sahul-yadav <yadavsahul220@gmail.com>
2026-04-01 21:31:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-black text-cyan-50 h-[100vh] w-full'>
+        <h1>sahul</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
